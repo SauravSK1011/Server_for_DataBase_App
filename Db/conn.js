dca794b47e57725c8f034275dceb48aa2b0263d8
Saurav <Sauravskamtalwar1011@gmail.com>
2022-03-24 14:21:32 +0530

    added

--- Db/conn.js
+++ Db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const DB =process.env.DB;
 
 mongoose
-  .connect(DB, {
+  .connect("mongodb+srv://ssk:sauravsk@cluster0.jwwkv.mongodb.net/mernstack?retryWrites=true&w=majority", {
   })
   .then(() => {
     console.log("connected");
